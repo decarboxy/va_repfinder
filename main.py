7288d8109c4872fb38139ec1fa74859cda43e057
Sam DeLuca <sam@decarboxy.com>
2015-12-06 11:43:53 -0500

    adding logging

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ import sys
 import csv
 import re
 from pyvirtualdisplay import Display
+import logging
 
 app = Flask(__name__)
+
+
+# Log only in production mode.
+if not app.debug:
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    app.logger.addHandler(stream_handler)
 
 
 class RepresentativeData():
